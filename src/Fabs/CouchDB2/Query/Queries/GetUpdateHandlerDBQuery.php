ac5b5a9d55ebb7ef51d3122c2f142ac1656f460e
Ahmet Türk <ahmetturk@Ahmets-iMac.local>
2017-03-07 16:31:18 +0300

    ADD GetUpdateHandler

--- src/Fabs/CouchDB2/Query/Queries/GetUpdateHandlerDBQuery.php
+++ src/Fabs/CouchDB2/Query/Queries/GetUpdateHandlerDBQuery.php
@@ -21,27 +21,37 @@ class GetUpdateHandlerDBQuery extends DBQuery
         $this->reset();
         $this->execution_method = 'update_handler';
         $this->query_method = QueryMethods::POST;
-        $this->allowed_response_codes = [QueryStatusCodes::SUCCESS];
+        $this->allowed_response_codes = [QueryStatusCodes::SUCCESS,QueryStatusCodes::CREATED];
         $this->query_url = sprintf('_design/%s/_update/%s', $design_doc_id, $update_handler_name);
+        $this->query_headers['Content-Type'] = 'application/x-www-form-urlencoded';
         parent::__construct($couch_object, $database_name);
     }
 
     public function selectDocument($document_id)
     {
         $this->query_method = QueryMethods::PUT;
-        $this->query_url = sprintf('_%s/%s', $this->query_url, $document_id);
+        $this->query_url = sprintf('%s/%s', $this->query_url, $document_id);
         return $this;
     }
 
-
+    /**
+     * @param $variable_name
+     * @param $value
+     * @return GetUpdateHandlerDBQuery
+     */
     public function setFormField($variable_name, $value)
     {
         $this->query_data[$variable_name] = $value;
         return $this;
     }
 
+    /**
+     * @param $variable_name
+     * @param $value
+     * @return GetUpdateHandlerDBQuery
+     */
     public function setQueryField($variable_name, $value)
     {
-        return $this->setQueryParameters($variable_name,$value);
+        return $this->setQueryParameters($variable_name, $value);
     }
 }
