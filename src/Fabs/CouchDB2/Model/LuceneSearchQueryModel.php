7019224c539d46c7058e420984818042e8c39179
omer <okocaoglu05@gmail.com>
2018-07-20 11:16:50 +0300

    ADD convertQueryValueToTurkishLowercase

--- src/Fabs/CouchDB2/Model/LuceneSearchQueryModel.php
+++ src/Fabs/CouchDB2/Model/LuceneSearchQueryModel.php
@@ -26,7 +26,7 @@ class LuceneSearchQueryModel
     function __construct($field, $value, $is_exact = true, $is_required = true)
     {
         $this->field = $field;
-        $this->value = $value;
+        $this->value = $this->convertQueryValueToTurkishLowercase($value);
         $this->is_exact = $is_exact;
         $this->is_required = $is_required;
     }
@@ -39,7 +39,7 @@ class LuceneSearchQueryModel
      */
     public function addAllowedSpecialCharacters($new_allowed_special_characters)
     {
-        foreach ($new_allowed_special_characters as $allowed_special_character){
+        foreach ($new_allowed_special_characters as $allowed_special_character) {
             $this->addAllowedSpecialCharacter($allowed_special_character);
         }
 
@@ -59,5 +59,24 @@ class LuceneSearchQueryModel
         }
 
         return $this;
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    private function convertQueryValueToTurkishLowercase($value)
+    {
+        $cleaned_char_list = [];
+        $char_lookup = ['I' => 'ı', 'İ' => 'i', 'Ş' => 'ş', 'Ö' => 'ö', 'Ğ' => 'ğ', 'Ü' => 'ü'];
+        $tmp_arr = preg_split('~~u', $value, -1, PREG_SPLIT_NO_EMPTY);
+        foreach ($tmp_arr as $chr) {
+            if (array_key_exists($chr, $char_lookup) === true) {
+                $cleaned_char_list[] = $char_lookup[$chr];
+            } else {
+                $cleaned_char_list[] = $chr;
+            }
+        }
+        return strtolower(implode($cleaned_char_list));
     }
 }
